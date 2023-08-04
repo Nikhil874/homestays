24ea8c13166772d9a1213077960813e42f3a5efb
jayant <ja@trinityagtech.com>
2023-08-04 14:18:53 +0530

    reverted cron jobs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const FloorController = require("./controller/floorController");
 const RoomController = require("./controller/roomController");
 const Controllers = require("./controller");
 const cors = require("cors");
-var cron = require("node-cron");
 const roomModel = require("./model/roomModel");
 const usersModel = require("./model/usersModel");
 require("dotenv").config();
@@ -14,22 +13,6 @@ const port = process.env.PORT || 1333;
 
 const app = express();
 app.use(cors());
-
-cron.schedule("0 12 1 * *",async () => {
-  try{
-    await usersModel.updateMany({},{paid:false});
-  }catch(e){
-    console.log(e);
-  }
-});
-
-// const corsOptions ={
-//   origin:'any',
-//   credentials:true,            //access-control-allow-credentials:true
-//   optionSuccessStatus:200,
-//   methods:"any"
-// }
-// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api/v1/pg", Controllers);
